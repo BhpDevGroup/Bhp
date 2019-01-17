f8a381d397a19c7387c5c06fde8d2ba0a0d41a88
SkyeBlack <296471989@qq.com>
2019-01-17 10:31:58 +0800

    auto update

--- bhp-cli/bhp-cli/Program.cs
+++ bhp-cli/bhp-cli/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Bhp.Shell;
 using Bhp.Wallets;
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
 
 namespace Bhp
 {
@@ -30,6 +35,9 @@ namespace Bhp
             var bufferSize = 1024 * 67 + 128;
             Stream inputStream = Console.OpenStandardInput(bufferSize);
             Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, bufferSize));
+
+            //if (UpdateVersion()) return;
+
             var mainService = new MainService();
             mainService.Run(args);
         }
@@ -52,6 +60,55 @@ namespace Bhp
                 writer.WriteLine();
                 PrintErrorLogs(writer, ex.InnerException);
             }
+        }
+
+        static string download_path;
+        private static bool UpdateVersion()
+        {
+            XDocument xdoc = null;
+            WebClient web = new WebClient();
+            string download_url;
+
+            try
+            {
+                xdoc = XDocument.Load("https://bhpa.io/client/update.xml");
+            }
+            catch { }
+            if (xdoc != null)
+            {
+                Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                Version minimum = Version.Parse(xdoc.Element("update").Attribute("minimum").Value);
+                if (version < minimum)
+                {
+                    Version latest = Version.Parse(xdoc.Element("update").Attribute("latest").Value);
+                    XElement release = xdoc.Element("update").Elements("release").First(p => p.Attribute("version").Value == latest.ToString());
+                    download_url = release.Attribute("file").Value;
+                    download_path = "Release.zip";
+                    web.DownloadFile(new Uri(download_url), download_path);
+                    UpdateFiles();
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static void UpdateFiles()
+        {
+            DirectoryInfo di = new DirectoryInfo("update");
+            if (di.Exists) di.Delete(true);
+            di.Create();
+            ZipFile.ExtractToDirectory(download_path, di.Name);
+            FileSystemInfo[] fs = di.GetFileSystemInfos();
+            if (fs.Length == 1 && fs[0] is DirectoryInfo)
+            {
+                ((DirectoryInfo)fs[0]).MoveTo("update2");
+                di.Delete();
+                Directory.Move("update2", di.Name);
+            }
+            //File.WriteAllBytes("update.bat", Resources.UpdateBat);
+            Process.GetCurrentProcess().Close();
+            Process.Start("update.bat");
         }
     }
 }

8da0ea2d2fca8477db1e8b915e8afe373e2c7578
SkyeBlack <296471989@qq.com>
2019-03-07 17:50:42 +0800

    arrangewallet programbar step double

--- bhp-gui/UI/ArrangeWalletDialog.cs
+++ bhp-gui/UI/ArrangeWalletDialog.cs
@@ -71,7 +71,7 @@ namespace Bhp.UI
                     return;
                 }
                 int loopNum = coins.Length / MaxInputCount + ((coins.Length % MaxInputCount == 0) ? 0 : 1);
-                double step = 100 / loopNum;
+                double step = (double)100 / loopNum;
 
                 for (int i = 0; i < loopNum; i++)
                 {

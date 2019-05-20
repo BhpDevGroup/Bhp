6de07180f10f54d4d4d761c0d23508e83a948d46
SkyeBlack <296471989@qq.com>
2019-05-20 11:04:16 +0800

    add AppCallContract

---
+++ smartcontractapp/AppCallContract/AppCallContract.cs
@@ -0,0 +1,14 @@
+﻿using Bhp.SmartContract.Framework;
+
+namespace AppCallContract
+{
+    public class AppCallContract : SmartContract
+    {
+        [Appcall("b6b31246a8fd11d7cd12ad3958106a8be1175aae")]
+        public static extern int Call(string operation);
+        public static object Main()
+        {
+            return Call("get");
+        }
+    }
+}

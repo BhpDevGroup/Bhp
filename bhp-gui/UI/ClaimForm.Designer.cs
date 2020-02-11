f41bc5f598d0675c952b4fb276e3fecc8bbdadd7
SkyeBlack <296471989@qq.com>
2020-02-11 15:18:44 +0800

    add claim gas address

--- bhp-gui/UI/ClaimForm.Designer.cs
+++ bhp-gui/UI/ClaimForm.Designer.cs
@@ -34,6 +34,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.combo_address = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -67,10 +69,25 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // combo_address
+            // 
+            this.combo_address.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combo_address.FormattingEnabled = true;
+            resources.ApplyResources(this.combo_address, "combo_address");
+            this.combo_address.Name = "combo_address";
+            this.combo_address.TextChanged += new System.EventHandler(this.combo_address_TextChanged);
+            // 
             // ClaimForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.combo_address);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -93,5 +110,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox combo_address;
     }
 }

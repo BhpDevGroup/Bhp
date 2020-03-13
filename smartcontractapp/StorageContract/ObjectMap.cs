1642e02c66388762cb2fd415fa37adc9b6feb9af
SkyeBlack <296471989@qq.com>
2020-03-13 14:05:02 +0800

    add object storage map

---
+++ smartcontractapp/StorageContract/ObjectMap.cs
@@ -0,0 +1,87 @@
+﻿using Bhp.SmartContract.Framework;
+using System.Numerics;
+
+namespace StorageContract
+{
+    public class ObjectMap
+    {
+        public AddressData[] addressData;
+        public static ObjectMap Deserialize(byte[] value)
+        {
+            ObjectMap objectMap = null;
+            if (value != null && value.Length != 0)
+            {
+                var obj = value.Deserialize();
+                objectMap = (ObjectMap)obj;
+            }
+            return objectMap;
+        }
+    }
+
+    public class AddressData
+    {
+        public string address;
+        public BasicData[] basic;
+        public BasicData[] advanced;
+
+        public static AddressData GetAddressData(ObjectMap value, string address)
+        {
+            if (value == null || value.addressData == null || value.addressData.Length == 0) return null;
+            for (int i = 0; i < value.addressData.Length; i++)
+            {
+                if (value.addressData[i].address == address)
+                {
+                    return value.addressData[i];
+                }
+            }
+            return null;
+        }
+
+        public static BasicData[] GetBasicData(AddressData value, AccountType type)
+        {
+            if (value == null) return null;
+            BasicData[] basicData = null;
+            switch (type)
+            {
+                case AccountType.Basic:
+                    if (value.basic != null && value.basic.Length > 0)
+                    {
+                        basicData = value.basic;
+                    }
+                    break;
+                case AccountType.Advance:
+                    if (value.advanced != null && value.advanced.Length > 0)
+                    {
+                        basicData = value.advanced;
+                    }
+                    break;
+            }
+            return basicData;
+        }
+    }
+
+    public class BasicData
+    {
+        public uint time;
+        public BigInteger value;
+
+        public static BasicData GetByTime(BasicData[] data, uint time)
+        {
+            if (data == null || data.Length == 0) return null;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i].time == time)
+                {
+                    return data[i];
+                }
+            }
+            return null;
+        }        
+    }
+
+    public enum AccountType
+    {
+        Basic = 1,
+        Advance = 2
+    }
+}
--- smartcontractapp/StorageContract/StorageContract.cs
+++ smartcontractapp/StorageContract/StorageContract.cs
@@ -1,5 +1,6 @@
 ﻿using Bhp.SmartContract.Framework;
 using Bhp.SmartContract.Framework.Services.Bhp;
+using StorageContract;
 
 namespace SotrageContract
 {
@@ -27,6 +28,7 @@ namespace SotrageContract
                     Storage.Delete(Storage.CurrentContext, "lock");
                     return true;
                 }
+                /****************** map *********************/
                 else if (operation == "putMap")
                 {
                     if (!Runtime.CheckWitness(owner)) return false;
@@ -54,6 +56,53 @@ namespace SotrageContract
                 else if (operation == "getMap5")
                 {
                     return Storage.Get(Storage.CurrentContext, address);//return null
+                }
+                /****************** object map *********************/
+                else if (operation == "putOmap")
+                {
+                    //if (!Runtime.CheckWitness(owner)) return false;
+                    BasicData basicData = new BasicData() { time = 1584002811, value = 123 };
+                    BasicData advancedData = new BasicData() { time = 1584069212, value = 321 };
+                    AddressData addressData = new AddressData { address = address, basic = new BasicData[] { basicData }, advanced = new BasicData[] { advancedData } };
+                    ObjectMap objectMap = new ObjectMap() { addressData = new AddressData[] { addressData } };
+                    Storage.Put(Storage.CurrentContext, "objMap", objectMap.Serialize());
+                    return objectMap.Serialize();
+                    //[{"type":"ByteArray","value":"800180018003002241546533774445394d5051585a7576686750524564514e596b69434246374a536859800181020004fbf6695e00017b8001810200045cfa6a5e00024101"}]
+                    //return true;
+                }
+                else if (operation == "getOmap")
+                {
+                    byte[] obj = Storage.Get(Storage.CurrentContext, "objMap");
+                    ObjectMap objectMap = ObjectMap.Deserialize(obj);
+                    return objectMap;
+                    //{"type":"Array","value":[{"type":"Array","value":[{"type":"Array","value":[{"type":"ByteArray","value":"41546533774445394d5051585a7576686750524564514e596b69434246374a536859"},{"type":"Array","value":[{"type":"Array","value":[{"type":"ByteArray","value":"fbf6695e"},{"type":"ByteArray","value":"7b"}]}]},{"type":"Array","value":[{"type":"Array","value":[{"type":"ByteArray","value":"5cfa6a5e"},{"type":"ByteArray","value":"4101"}]}]}]}]}]}]
+                }
+                else if (operation == "getOmap2")
+                {
+                    byte[] obj = Storage.Get(Storage.CurrentContext, "objMap");
+                    ObjectMap objectMap = ObjectMap.Deserialize(obj);
+                    AddressData addressData = AddressData.GetAddressData(objectMap, address);
+                    return addressData;
+                    //[{"type":"Array","value":[{"type":"ByteArray","value":"41546533774445394d5051585a7576686750524564514e596b69434246374a536859"},{"type":"Array","value":[{"type":"Array","value":[{"type":"ByteArray","value":"fbf6695e"},{"type":"ByteArray","value":"7b"}]}]},{"type":"Array","value":[{"type":"Array","value":[{"type":"ByteArray","value":"5cfa6a5e"},{"type":"ByteArray","value":"4101"}]}]}]}]
+                }
+                else if (operation == "getOmap3")
+                {
+                    byte[] obj = Storage.Get(Storage.CurrentContext, "objMap");
+                    ObjectMap objectMap = ObjectMap.Deserialize(obj);
+                    AddressData addressData = AddressData.GetAddressData(objectMap, address);
+                    BasicData[] basics = AddressData.GetBasicData(addressData, AccountType.Basic);
+                    return basics;
+                    //[{"type":"Array","value":[{"type":"Array","value":[{"type":"ByteArray","value":"fbf6695e"},{"type":"ByteArray","value":"7b"}]}]}]
+                }
+                else if (operation == "getOmap4")
+                {
+                    byte[] obj = Storage.Get(Storage.CurrentContext, "objMap");
+                    ObjectMap objectMap = ObjectMap.Deserialize(obj);
+                    AddressData addressData = AddressData.GetAddressData(objectMap, address);
+                    BasicData[] basics = AddressData.GetBasicData(addressData, AccountType.Basic);
+                    BasicData data = BasicData.GetByTime(basics, 1584002811);
+                    return data;
+                    //[{"type":"Array","value":[{"type":"ByteArray","value":"fbf6695e"},{"type":"ByteArray","value":"7b"}]}]
                 }
             }
             return false;

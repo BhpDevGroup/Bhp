72ad40924b26e983a38a0b284e4f1147ead3b968
SkyeBlack <296471989@qq.com>
2020-05-20 14:45:32 +0800

    bhp-gui-light

--- bhp-gui-light/Properties/AssemblyInfo.cs
+++ bhp-gui-light/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("bhp-gui")]
+[assembly: AssemblyTitle("bhp-gui-light")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("bhp-gui")]
+[assembly: AssemblyProduct("bhp-gui-light")]
 [assembly: AssemblyCopyright("© The Bhp Project 2018-2019 Released under the MIT license")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -30,5 +30,4 @@ using System.Runtime.InteropServices;
 //
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
-[assembly: AssemblyVersion("1.2.1.1")]
-//[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]

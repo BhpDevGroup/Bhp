993a77b7326c196865c77ced87155f9a82b9de3c
Reskorey <1204490370@qq.com>
2020-06-05 11:45:02 +0800

    add Encrypt Remark

--- bhp-gui-light/UI/InputBox.Designer.cs
+++ bhp-gui-light/UI/InputBox.Designer.cs
@@ -33,7 +33,17 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_key = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
@@ -43,24 +53,75 @@
             // 
             // groupBox1
             // 
-            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.textBox1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.TabStop = false;
             // 
             // button1
             // 
-            resources.ApplyResources(this.button1, "button1");
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            resources.ApplyResources(this.button2, "button2");
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.button2, "button2");
             this.button2.Name = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_key);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // txt_key
+            // 
+            resources.ApplyResources(this.txt_key, "txt_key");
+            this.txt_key.Name = "txt_key";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox7);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // textBox7
+            // 
+            resources.ApplyResources(this.textBox7, "textBox7");
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Name = "label1";
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Name = "label2";
             // 
             // InputBox
             // 
@@ -68,6 +129,12 @@
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.button2;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
@@ -78,7 +145,12 @@
             this.ShowInTaskbar = false;
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -88,5 +160,13 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_key;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
     }
 }
--- bhp-gui-light/UI/InputBox.cs
+++ bhp-gui-light/UI/InputBox.cs
@@ -1,4 +1,8 @@
 ﻿using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using System;
 
 namespace Bhp.UI
 {
@@ -17,8 +21,83 @@ namespace Bhp.UI
             using (InputBox dialog = new InputBox(text, caption, content))
             {
                 if (dialog.ShowDialog() != DialogResult.OK) return null;
+
+                if (!string.IsNullOrEmpty(dialog.textBox7.Text)) 
+                {
+                    return "cipertext" + dialog.textBox7.Text;
+                }
                 return dialog.textBox1.Text;
             }
+        }
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            string stringToEncrypt = textBox1.Text;
+            string key = txt_key.Text;
+
+            if (string.IsNullOrEmpty(stringToEncrypt))
+            {
+                MessageBox.Show("请输入备注！");
+                return;
+            }
+            if (string.IsNullOrEmpty(key)) 
+            {
+                MessageBox.Show("密钥必须为八位字符！");
+                return;
+            }
+            if (key.Length != 8)
+            {
+                MessageBox.Show("密钥必须为八位字符！");
+                return;
+            }
+
+            this.textBox7.Text = Encrypt(stringToEncrypt, key);
+        }
+
+
+        public static string Encrypt(string stringToEncrypt, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray = Encoding.GetEncoding("UTF-8").GetBytes(stringToEncrypt);
+            des.Key = ASCIIEncoding.UTF8.GetBytes(sKey);
+            des.IV = ASCIIEncoding.UTF8.GetBytes(sKey);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in ms.ToArray())
+            {
+                ret.AppendFormat("{0:X2}", b);
+            }
+            ret.ToString();
+            return ret.ToString();
+        }
+
+
+        public static string Decrypt(string stringToDecrypt, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray = new byte[stringToDecrypt.Length / 2];
+            for (int x = 0; x < stringToDecrypt.Length / 2; x++)
+            {
+                int i = (Convert.ToInt32(stringToDecrypt.Substring(x * 2, 2), 16));
+                inputByteArray[x] = (byte)i;
+            }
+            des.Key = ASCIIEncoding.UTF8.GetBytes(sKey);
+            des.IV = ASCIIEncoding.UTF8.GetBytes(sKey);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            return System.Text.Encoding.Default.GetString(ms.ToArray());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string dekeyString =  Decrypt(textBox7.Text, txt_key.Text);
+            MessageBox.Show(dekeyString);
         }
     }
 }
